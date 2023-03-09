5e49a5808f66c901d169a694359466eaee2966a4
VladimirTarasovets <tarasovets21@gmail.com>
2023-03-09 13:40:24 +0200

    upd

--- HW2.sql
+++ HW2.sql
@@ -185,26 +185,32 @@ from classicmodels.customers
 where city in ('Melbourne');
 -- show all customers with Credit Limit over $200,000
 select * from classicmodels.customers 
-where creditLimit>=200000;
+where creditLimit>200000;
 -- who is the president of the company?
 select * from classicmodels.employees
 where jobTitle like '%president%';
 -- how many Sales Reps are in the company?
 select count(*) from classicmodels.employees
-where jobTitle like '%Sales Rep%';
+where jobTitle like 'Sales%Rep%';
 -- show payments in descending order
 select * from classicmodels.payments
 order by amount desc;
 -- what was the check# for the payment done on December 17th 2004
-select * from classicmodels.payments
+select checkNumber, paymentDate from classicmodels.payments
 where paymentDate = '2004-12-17';
 -- show product line with the word 'realistic' in the description
-select * from classicmodels.products
-where productDescription like '%realistic%';
+select productLine, textDescription from classicmodels.productlines
+where textDescription like '%realistic %';
 -- show product name for vendor 'Unimax Art Galleries'
-select * from classicmodels.products
+select productName, productVendor from classicmodels.products
 where productVendor = 'Unimax Art Galleries';
 -- what is the customer number for the highest amount of payment
-select max(amount), customerNumber from classicmodels.payments;
+select max(amount), customerNumber from classicmodels.payments
+group by customerNumber
+limit 1;
+
+select customerNumber, amount from classicmodels.payments
+order by amount desc
+limit 1;
 -- Save your work 
  -- ------------------------------------
